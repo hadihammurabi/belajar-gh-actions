f614c6f2be70f891499529b1992d5447a2f482a1
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-04-22 15:49:31 +0700

    update test

--- test/product.spec.js
+++ test/product.spec.js
@@ -6,7 +6,7 @@ describe('/api/products', () => {
     try {
       const response = await axios.get('https://reqres.in/api/products');
       const { data } = response;
-      expect(data.data.length).toBe(data.per_page);
+      expect(data.data.length).toBe(3);
     } catch (error) {
       expect(error).toBe(null);
     }
